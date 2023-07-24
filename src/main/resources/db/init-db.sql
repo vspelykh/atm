101e7a43cfe2c6b1302683e1411badbf5a70c468
Vladyslav Pelykh <vspelykh@gmail.com>
2023-07-24 21:49:38 +0300

    Fix DB schema
    Signed-off-by: Vladyslav Pelykh <vspelykh@gmail.com>

--- src/main/resources/db/init-db.sql
+++ src/main/resources/db/init-db.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS withdrawals;
 DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS atms;
@@ -6,36 +7,36 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
     id           SERIAL PRIMARY KEY,
-    first_name   VARCHAR(50)         NOT NULL,
-    last_name    VARCHAR(50)         NOT NULL,
-    address      VARCHAR(100),
-    phone_number VARCHAR(20),
-    email        VARCHAR(100) UNIQUE NOT NULL,
-    role         VARCHAR(20)         NOT NULL
+    first_name   VARCHAR(50)        NOT NULL,
+    last_name    VARCHAR(50)        NOT NULL,
+    phone_number VARCHAR(20) UNIQUE NOT NULL,
+    role         VARCHAR(20)        NOT NULL
 );
 
 CREATE TABLE accounts
 (
-    id            SERIAL PRIMARY KEY,
-    account_number VARCHAR(20) NOT NULL,
+    id             SERIAL PRIMARY KEY,
+    account_number VARCHAR(20)    NOT NULL,
     balance        NUMERIC(12, 2) NOT NULL,
     user_id        INTEGER REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    password       VARCHAR(100) NOT NULL,
-    issue_date     DATE NOT NULL
+    password       VARCHAR(100)   NOT NULL,
+    issue_date     DATE           NOT NULL
 );
 
-CREATE INDEX idx_accounts_account_number ON accounts (account_number);
+CREATE UNIQUE INDEX idx_accounts_account_number ON accounts (account_number);
+CREATE INDEX idx_accounts_user_id ON accounts (user_id);
 
 CREATE TABLE transactions
 (
-    id                 SERIAL PRIMARY KEY,
-    transaction_type   VARCHAR(20)    NOT NULL,
-    amount             NUMERIC(12, 2) NOT NULL,
-    transaction_date   TIMESTAMP      NOT NULL,
-    source_account     VARCHAR(20),
-    destination_account VARCHAR(20),
-    user_id            INTEGER REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+    id                     SERIAL PRIMARY KEY,
+    amount                 NUMERIC(12, 2) NOT NULL,
+    transaction_date       TIMESTAMP      NOT NULL,
+    source_account_id      INTEGER REFERENCES accounts (id),
+    destination_account_id INTEGER REFERENCES accounts (id),
+    user_id                INTEGER REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE INDEX idx_transactions_user_id ON transactions (user_id);
 
 CREATE TABLE atms
 (
@@ -43,3 +44,15 @@ CREATE TABLE atms
     location     VARCHAR(100) NOT NULL,
     availability VARCHAR(3)   NOT NULL
 );
+
+CREATE TABLE withdrawals
+(
+    id               SERIAL PRIMARY KEY,
+    amount           NUMERIC(12, 2)          NOT NULL,
+    transaction_date TIMESTAMP DEFAULT NOW() NOT NULL,
+    account_id       INTEGER REFERENCES accounts (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    atm_id           INTEGER REFERENCES atms (id),
+    user_id          INTEGER REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE INDEX idx_withdrawals_account_id ON withdrawals (account_id);
--- src/main/resources/db/populate-db.sql
+++ src/main/resources/db/populate-db.sql
@@ -1,17 +1,17 @@
-INSERT INTO users (first_name, last_name, address, phone_number, email, role)
-VALUES ('John', 'Doe', '123 Main St, New York', '+1 123-456-7890', 'john.doe@example.com', 'user'),
-       ('Alice', 'Smith', '456 Park Ave, San Francisco', '+1 987-654-3210', 'alice.smith@example.com', 'user'),
-       ('Bob', 'Johnson', '789 Broadway, Los Angeles', '+1 555-123-4567', 'bob.johnson@example.com', 'user');
+INSERT INTO users (first_name, last_name, phone_number, role)
+VALUES ('John', 'Doe', '+1 123-456-7890', 'user'),
+       ('Alice', 'Smith', '+1 987-654-3210', 'user'),
+       ('Bob', 'Johnson', '+1 555-123-4567', 'user');
 
 INSERT INTO accounts (account_number, balance, user_id, password, issue_date)
-VALUES ('1234567890123456', 5000.00,  1, 'pass123', '2023-07-23'),
+VALUES ('1234567890123456', 5000.00, 1, 'pass123', '2023-07-23'),
        ('9876543210987654', 3000.00, 2, 'pass456', '2023-07-23'),
        ('1111222233334444', 10000.00, 3, 'pass789', '2023-07-23');
 
-INSERT INTO transactions (transaction_type, amount, transaction_date, source_account, destination_account, user_id)
-VALUES ('debit', 1000.00, '2023-07-23 10:30:00', '1234567890123456', NULL, 1),
-       ('credit', 2000.00, '2023-07-23 11:15:00', NULL, '9876543210987654', 2),
-       ('transfer', 500.00, '2023-07-23 12:00:00', '1111222233334444', '1234567890123456', 3);
+INSERT INTO transactions (amount, transaction_date, source_account_id, destination_account_id, user_id)
+VALUES (1000.00, '2023-07-23 10:30:00', 1, NULL, 1),
+       (2000.00, '2023-07-23 11:15:00', NULL, 2, 2),
+       (500.00, '2023-07-23 12:00:00', 3, 1, 3);
 
 INSERT INTO atms (location, availability)
 VALUES ('New York, 5th Avenue', 'Yes'),
