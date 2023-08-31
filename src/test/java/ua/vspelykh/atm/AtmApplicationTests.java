3f95aa841e321f6162638b801bc18c34957c952a
Vladyslav Pelykh <vspelykh@gmail.com>
2023-08-31 11:55:46 +0300

    Add tips to log in
    Signed-off-by: Vladyslav Pelykh <vspelykh@gmail.com>

--- src/test/java/ua/vspelykh/atm/AtmApplicationTests.java
+++ src/test/java/ua/vspelykh/atm/AtmApplicationTests.java
@@ -3,11 +3,21 @@ package ua.vspelykh.atm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AtmApplicationTests {
 
     @Test
     void contextLoads() {
+
+       Thread t = new Thread(){
+           @Override
+           public void run() {
+               for (int i = 1; i < 3; i++) {
+                   
+               System.out.print(i + "..");
+               }
+           }
+       };
+       t.run();
     }
 
 }
